a144bb39383441721be8ba6ede909bb928738dce
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-01-25 21:46:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = { new ArgumentException(), new FormatException(), new ArgumentNullException(), new OverflowException(), new MyException() };
+            foreach (Exception ex in exceptions)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
